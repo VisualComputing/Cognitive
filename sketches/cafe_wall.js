4aa2f4e4c1a2de24770042dbcbdb9303b56a3783
Jairo Suarez <jasuarezpr@gmail.com>
2017-02-23 10:41:32 -0500

    moved mouse movement event to the setup function and added some documentation

--- sketches/cafe_wall.js
+++ sketches/cafe_wall.js
@@ -1,23 +1,28 @@
 // Author: Jairo Suarez
 // February 16, 2017
-var cWidth = 30;
-var cHeight = 26;
-var linexHeight = 1;
-var lineyHeight = 2;
-var rows = 9;
+var cWidth = 30; // cell width
+var cHeight = 26; // cell height
+var linexHeight = 1; // stroke width for horizontal
+var lineyHeight = 2; // stroke width for vertical lines
+var rows = 9; // number of rows to draw
+var canvasWidth = 400; // canvas width
 
 var sketch2 = function(p) {
     p.setup = function() {
-        p.createCanvas(400, (cHeight + lineyHeight) * rows + lineyHeight);
+        p.createCanvas(canvasWidth, (cHeight + lineyHeight) * rows + lineyHeight);
         p.background('#888');
-        p.strokeWeight(3);
         p.smooth();
         p.noLoop();
+        // draw the cell each time the mouse is moved
+        p.mouseMoved = function() {
+            p.draw();
+        }
     };
 
+    // draw a row of cells
     function drawRow(row) {
         yPos = row * (cHeight + lineyHeight) + lineyHeight;
-        numCells = Math.ceil(400 / cWidth) + 3;
+        numCells = Math.ceil(canvasWidth / cWidth) + 3;
         for (var i = -80; i < numCells; i = i + 1) {
             if (i % 2 == 0) {
                 p.fill(0);
@@ -36,10 +41,7 @@ var sketch2 = function(p) {
         }
     }
 
-    p.mouseMoved = function() {
-        p.draw();
-    };
-
+    // draw the rows
     p.draw = function() {
         p.background('#888');
         for (var i = 0; i < rows; i = i + 1) {
