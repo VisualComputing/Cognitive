646c2a569fc4a16600afb058ea873abf8fd7e2de
Jairo Suarez <jasuarezpr@gmail.com>
2017-02-23 10:30:43 -0500

    adding documentation for color gradient

--- sketches/color_gradient.js
+++ sketches/color_gradient.js
@@ -1,29 +1,34 @@
 // Author: Jairo Suarez
 // February 16, 2017
-var c1,c2,c3;
+var c1,c2,c3,w,h;
 
 var sketch3 = function(p) {
     p.setup = function() {
-        p.createCanvas(440, 320);
+        w = 440; // canvas width
+        h = 320; // canvas height
+        // Here we use [Web Colors](https://en.wikipedia.org/wiki/Web_colors) using the #RRGGBB model
         c1 = "#323232"; // left color
         c2 = "#C7C7C7"; // right color
         c3 = "#787878"; // block color
+        p.createCanvas(w,h);
         p.smooth();
         p.noLoop();
     };
 
     // adapted from: https://p5js.org/examples/color-linear-gradient.html
-    function setGradient(x, y, w, h, c1, c2) {
+    // this function draws a left to right gradient interpolating the values between two colors (c1,c2)
+    function setGradient(c1, c2) {
         p.noFill();
         // Left to right gradient
-        for (var i = x; i <= x + w; i++) {
-            var inter = p.map(i, x, x + w, 0, 1);
+        for (var i = 0; i <=  w; i++) {
+            var inter = p.map(i, 0, w, 0, 1);
             var c = p.lerpColor(c1, c2, inter);
             p.stroke(c);
-            p.line(i, y, i, y + h);
+            p.line(i, 0, i, h);
         }
     }
 
+    // this sets a listener to the mouse click function and toggles the gradient
     p.mouseClicked = function() {
         if (c2 == "#C7C7C7") {
             c2 = "#323232";
@@ -34,7 +39,7 @@ var sketch3 = function(p) {
     };
 
     p.draw = function() {
-        setGradient(0, 0, p.width, p.height, c1, c2);
+        setGradient( c1, c2);
         p.noStroke();
         p.fill(c3);
         p.rect(30, 140, 380, 40);
