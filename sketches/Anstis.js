c55882c0fe64b2c3a89102016c6b51b29dd67f82
Daniel <Daniel>
2017-02-23 21:05:56 -0500

    Fixed Anstis size

--- sketches/Anstis.js
+++ sketches/Anstis.js
@@ -3,17 +3,13 @@ var rspeed = 1.00;
 
 var sketch6 = function( p ) {
     p.setup = function() {
-      var myCanvas = p.createCanvas(800, 600);
+      var myCanvas = p.createCanvas(600, 400);
       myCanvas.parent( 'Anstis_id' );
       p.background(255);
     };
 
     p.draw = function() {
       p.background(255);
-      p.noFill();
-      p.strokeWeight(5);
-      p.stroke(0);
-      p.rect( 100, 100, 600, 400 );
       if( !p.mouseIsPressed )
         makeBars();
       moveRect();
@@ -22,18 +18,18 @@ var sketch6 = function( p ) {
     function moveRect() {
       p.noStroke();
       p.fill(255,255,0);
-      p.rect( rx,220, 60,30 );
+      p.rect( rx,120, 60,30 );
       p.fill(0,0,102);
-      p.rect( rx,320, 60,30 );
+      p.rect( rx,220, 60,30 );
       rx += rspeed;
-      if( rx+60 >= 700 || rx <= 100 ) rspeed *= (-1.0);
+      if( rx+60 >= 600 || rx <= 10 ) rspeed *= (-1.0);
     }
 
     function makeBars() {
       p.strokeCap('SQUARE');
       p.strokeWeight(15);
       for(var i = 0; i < 20; i++) 
-        p.line(110+i*30,495,110+i*30,105);
+        p.line(10+i*30,495-100,10+i*30,5);
     }
 
 };
