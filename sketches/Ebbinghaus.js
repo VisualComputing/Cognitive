aec73dd5f0a648bab4d80ca644c116d13b26693e
Daniel <Daniel>
2017-02-23 21:09:31 -0500

    Fixed Ebbinghaus size

--- sketches/Ebbinghaus.js
+++ sketches/Ebbinghaus.js
@@ -1,6 +1,6 @@
 var sketch3 = function( p ) {
     p.setup = function() {
-        var myCanvas = p.createCanvas(800, 600);
+        var myCanvas = p.createCanvas(800, 400);
         myCanvas.parent( 'Ebbinghaus_id' );
         p.background(127);
     };
@@ -10,26 +10,26 @@ var sketch3 = function( p ) {
       p.noStroke();
       
       p.fill(225,128,0);
-      p.ellipse( 250,350, 60, 60 );
-      p.ellipse( 650,350, 60, 60 );
+      p.ellipse( 250,200, 60, 60 );
+      p.ellipse( 650,200, 60, 60 );
       
       if( !p.mouseIsPressed ) {
         p.fill(153,204,255);
-        p.ellipse( 100,350, 135, 135 );
-        p.ellipse( 177,470, 135, 135 );
-        p.ellipse( 323,470, 135, 135 );
-        p.ellipse( 400,350, 135, 135 );
-        p.ellipse( 177,220, 135, 135 );
-        p.ellipse( 323,220, 135, 135 );
+        p.ellipse( 100,200, 135, 135 );
+        p.ellipse( 177,320, 135, 135 );
+        p.ellipse( 323,320, 135, 135 );
+        p.ellipse( 400,200, 135, 135 );
+        p.ellipse( 177,70, 135, 135 );
+        p.ellipse( 323,70, 135, 135 );
         
-        p.ellipse( 600,350, 25, 25 );
-        p.ellipse( 615,385, 25, 25 );
-        p.ellipse( 650,400, 25, 25 );
-        p.ellipse( 685,385, 25, 25 );
-        p.ellipse( 700,350, 25, 25 );
-        p.ellipse( 685,315, 25, 25 );
-        p.ellipse( 650,300, 25, 25 );
-        p.ellipse( 615,315, 25, 25 );
+        p.ellipse( 600,200, 25, 25 );
+        p.ellipse( 615,235, 25, 25 );
+        p.ellipse( 650,250, 25, 25 );
+        p.ellipse( 685,235, 25, 25 );
+        p.ellipse( 700,200, 25, 25 );
+        p.ellipse( 685,165, 25, 25 );
+        p.ellipse( 650,150, 25, 25 );
+        p.ellipse( 615,165, 25, 25 );
       }
     };
 }
