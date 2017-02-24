a021dd8d353c85000c34a390473b48281fcf5698
Daniel <Daniel>
2017-02-23 21:02:11 -0500

    Fixed Zollner size

--- sketches/Zollner.js
+++ sketches/Zollner.js
@@ -1,31 +1,28 @@
 var sketch4 = function( p ) {
     p.setup = function() {
-        var myCanvas = p.createCanvas(800, 700);
+        var myCanvas = p.createCanvas(600, 400);
         myCanvas.parent( 'Zollner_id' );
         p.background(255);
     };
 
     p.draw = function() {
       p.background(255);
-      p.noFill();
-      p.stroke(0);
-      p.strokeWeight(12.5);
-      p.rect(100,100, 600, 500);
       makeParallelLines();
       if( !p.mouseIsPressed ) {
         makeIlussionLines();
       }
-      cleaner();
+      //cleaner();
     };
 
     function makeParallelLines( ) {
       p.strokeWeight(15);
       p.strokeCap('SQUARE');
-      var x = [100,200];
+      var aux = 100;
+      var x = [100-aux,200-aux];
       var y = [500, 600];
       var d = 100;
       
-      for(var i = 0; i < 10; i++) {
+      for(var i = 0; i < 11; i++) {
         p.line(x[0],y[0]-i*d, x[1]+i*d,y[1]);
       }
     }
@@ -37,24 +34,24 @@ var sketch4 = function( p ) {
     }
 
     function makeHorizontalLines() {
-      var qx = [170, 230, 215, 155];
+      var qx = [70, 130, 115, 55];
       var qy = [600,600,585,585];
       p.strokeWeight(1);
       p.fill(0);
-      for(var i = 0; i < 5; i++) {
-        for(var j = 0; 100 <= qx[0]+(200*i)-(j*30); j++) {
-          p.quad(qx[0]+(200*i)-(j*30), qy[0]-(j*30), qx[1]+(200*i)-(j*30), qy[1]-(j*30), qx[2]+(200*i)-(j*30), qy[2]-(j*30), p.max(100,qx[3]+(200*i)-(j*30)), qy[3]-(j*30));
+      for(var i = 0; i < 6; i++) {
+        for(var j = 0; 0 <= qx[0]+(200*i)-(j*30); j++) {
+          p.quad(qx[0]+(200*i)-(j*30), qy[0]-(j*30), qx[1]+(200*i)-(j*30), qy[1]-(j*30), qx[2]+(200*i)-(j*30), qy[2]-(j*30), qx[3]+(200*i)-(j*30), qy[3]-(j*30));
         }
       }
       
     }
 
     function makeVerticalLines() {
-      var qx = [100,100,115,115];
+      var qx = [0, 0, 15, 15];
       var qy = [430, 370, 385, 445];
       p.strokeWeight(1);
       p.fill(0);
-      for(var i = 0; i < 5; i++) {
+      for(var i = 0; i < 6; i++) {
         for(var j = 0; 610 >= qy[3]+(j*30)-(200*i); j++) {
           p.quad(qx[0]+(j*30), qy[0]+(j*30)-(200*i), qx[1]+(j*30), qy[1]+(j*30)-(200*i), qx[2]+(j*30), qy[2]+(j*30)-(200*i), qx[3]+(j*30), qy[3]+(j*30)-(200*i));
         }
